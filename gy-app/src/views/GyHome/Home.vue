dc217a084bb9a86c8874605cb0adefaf1ef9f769
geyanya <17602002374@163.com>
2020-12-16 20:56:24 +0800

    葛妍修改了main的GyHome文件夹

---
+++ gy-app/src/views/GyHome/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>葛妍创建的HOME组件</h1>
+        <p>哈哈哈哈哈哈</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
